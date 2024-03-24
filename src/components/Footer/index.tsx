47f2f33bb860b514347f34049a14f6a69dd56d7b
dung.kimthanh <dung.kimthanh@amela.vn>
2024-03-24 22:06:05 +0700

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import {CopyrightOutlined } from "@ant-design/icons"
-import styles from "./Style.module.scss"
-import { Link } from 'react-router-dom'
+import React from "react";
+import { CopyrightOutlined } from "@ant-design/icons";
+import styles from "./Style.module.scss";
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <div className={styles.wrap}>
-        Thiết kế và phát triển bởi {" "}
-        <Link to={'https://zalo.me/0978131878'} style={{color: "#f6c404", marginLeft:"8px", fontWeight: "600"}}>
-          Chippisoft
-        </Link>
+      <span className={styles.footerText}> Thiết kế và phát triển bởi</span>{" "}
+      <Link
+        to={"https://zalo.me/0978131878"}
+        style={{ color: "#f6c404", marginLeft: "8px", fontWeight: "600" }}
+      >
+        Chippisoft.
+      </Link>
     </div>
-  )
+  );
 }
