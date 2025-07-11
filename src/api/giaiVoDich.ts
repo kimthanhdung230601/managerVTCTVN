dd7e57b88ced49bfc713679d7d7a18c0c1b0cf29
dung kim <kim.thanh.dung@vsi-international.com>
2025-07-11 19:32:26 +0700

    feat: Update API endpoints in giaiVoDich and add CustomTableWeightTwo component

--- src/api/giaiVoDich.ts
+++ src/api/giaiVoDich.ts
@@ -1,24 +1,24 @@
 import { sendGet, sendPost } from "./api";
 
-export const getListInfor = () => sendGet("YoungFight");
+export const getListInfor = () => sendGet("ChampionFight");
 
 export const getListInforAdmin = (payload: any) =>
-  sendGet(`CubFight?idclub=${payload}`);
+  sendGet(`ChampionFight?idclub=${payload}`);
 
 export const addNewMember = (payload: any) =>
-  sendPost("CubFight?type=Submit&mode=2", payload);
+  sendPost("ChampionFight?type=Submit&mode=2", payload);
 
 export const getManagamentMember = (payload?: any) =>
-  sendPost("CubFight?type=Detail", payload ? payload : {});
+  sendPost("ChampionFight?type=Detail", payload ? payload : {});
 
 export const postFixF0 = (payload: any) =>
-  sendPost("CubFight?type=Fix", payload);
+  sendPost("ChampionFight?type=Fix", payload);
 export const updateFile = (payload: any, value: number) =>
-  sendPost(`CubFight?type=Image&mode=${value}`, payload);
+  sendPost(`ChampionFight?type=Image&mode=${value}`, payload);
 
 export const F0AddNewMember = (payload: any, idclub: any): Promise<any> => {
   return new Promise((resolve, reject) => {
-    sendPost(`CubFight?type=AdminSubmit&mode=2&idclub=${idclub}`, payload)
+    sendPost(`ChampionFight?type=AdminSubmit&mode=2&idclub=${idclub}`, payload)
       .then((response) => {
         resolve(response); // Resolve Promise khi API trả về thành công
       })
@@ -29,27 +29,29 @@ export const F0AddNewMember = (payload: any, idclub: any): Promise<any> => {
 };
 
 export const getListSubcribe = (param: any) =>
-  sendPost(`/CubFight?type=Detail`, param);
+  sendPost(`/ChampionFight?type=Detail`, param);
 
 export const updateListSubcribe = (param: any) =>
-  sendPost(`/CubFight?type=Fix`, param);
+  sendPost(`/ChampionFight?type=Fix`, param);
 
 export const getListMemberClub = (idclub: number) =>
-  sendGet(`/CubFight?idclub=${idclub}`);
+  sendGet(`/ChampionFight?idclub=${idclub}`);
 
 export const acceptClubSubscribe = (param: any) =>
-  sendPost(`/CubFight?type=Accept`, param);
+  sendPost(`/ChampionFight?type=Accept`, param);
 
-export const getListAcceptSubscribe = () => sendPost(`/CubFight?type=List`, {});
+export const getListAcceptSubscribe = () =>
+  sendPost(`/ChampionFight?type=List`, {});
 export const submitListmember = (idclub: any, param: any) =>
-  sendPost(`/CubFight?type=AdminSubmit&mode=1&idclub=${idclub}`, param);
-export const getListMemberClubF2 = () => sendGet("/CubFight");
+  sendPost(`/ChampionFight?type=AdminSubmit&mode=1&idclub=${idclub}`, param);
+export const getListMemberClubF2 = () => sendGet("/ChampionFight");
 export const submitListmemberF2 = (param: any) =>
-  sendPost("/CubFight?type=Submit&mode=1", param);
+  sendPost("/ChampionFight?type=Submit&mode=1", param);
 
 export const getInfoF2 = (param: any) => {
-  if (param != undefined) return sendPost("/CubFight?type=Info", { id: param });
-  else return sendPost("/CubFight?type=Info", {});
+  if (param != undefined)
+    return sendPost("/ChampionFight?type=Info", { id: param });
+  else return sendPost("/ChampionFight?type=Info", {});
 };
 
 // đăng ký giải vô địch
